3a3c878efd111f290e6fcca2e33f921a9ed13d3a
RusuRares <rush.nbh@gmail.com>
2017-08-20 18:07:47 +0300

    commit cu proiectul gol

---
+++ rushu/src/main/java/com/rushmeup/dezvAplicInternet/rushu/RushuApplication.java
@@ -0,0 +1,12 @@
+package com.rushmeup.dezvAplicInternet.rushu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RushuApplication.class, args);
+	}
+}
